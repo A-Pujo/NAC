2ddff01d059c2f0de8a7b2e090a0169dec8a940f
akhsanfr <akhsan.fr@gmail.com>
2021-09-02 11:13:48 +0700

    update datatable

--- app/Views/dashboard/layout/datatables.php
+++ app/Views/dashboard/layout/datatables.php
@@ -1,6 +1,4 @@
 <!-- Datatables -->
-
-    <!-- <link rel="stylesheet" href="/css/datatables.css"> -->
     <!-- Main Script -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.js"></script>
