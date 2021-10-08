240b598633601e5dbb6212ec0b284b4bb2dee0ae
A-Pujo <68434815+A-Pujo@users.noreply.github.com>
2021-10-08 20:05:48 +0700

    Update Lomba.php

--- app/Controllers/Lomba.php
+++ app/Controllers/Lomba.php
@@ -27,6 +27,10 @@ class Lomba extends BaseController
 
 	// dashboard perlombaan
 	public function dashboard(){
+		if(!isLoggedIn()){
+			return redirect()->to(base_url('dashboard'));
+		}
+
 		$data=[
 			'judul' => 'Perlombaan NAC 2021',
             'halaman' => 'lomba',
