5a16975a0c5c46b7750c7468e2c3a5d32d3dea1b
akhsanfr <akhsan.fr@gmail.com>
2021-10-09 10:41:52 +0700

    apdet  notif home

--- app/Views/statis/pages/home.php
+++ app/Views/statis/pages/home.php
@@ -22,7 +22,20 @@
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>                          
                 </svg> 
-                <label>Jadwal pelaksanaan Preliminary Round pada hari Minggu pukul 09:30 - 10:30</label>
+                <label>Persiapan Preliminary Round dilaksanakan pada hari Minggu, 10 Oktober 2021 pukul 09:00 - 09:30. Peserta dapat mengambil voucher Preliminary Round pada Dashboard masing-masing. </label>
+            </div>
+            <svg
+                @click="active = false"
+                xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="alert alert-info" x-data="{active: true}" x-show="active">
+            <div class="flex-1">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>                          
+                </svg> 
+                <label>Pengerjaan soal Preliminary Round dilaksanakan pada hari Minggu, 10 Oktober 2021 pukul 09:30 - 10:30</label>
             </div>
             <svg
                 @click="active = false"
