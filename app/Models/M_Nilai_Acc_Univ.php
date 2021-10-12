e4c18e61547e509e16b35ce39a5999678efbfd8a
A-Pujo <68434815+A-Pujo@users.noreply.github.com>
2021-10-12 13:32:18 +0700

    sorting tabel nilai

--- app/Models/M_Nilai_Acc_Univ.php
+++ app/Models/M_Nilai_Acc_Univ.php
@@ -30,7 +30,7 @@ class M_Nilai_Acc_Univ extends Model
         ->join('data_partisipan', 'data_partisipan.partisipan_id = nilai_acc_univ.partisipan_id')
         ->orderBy('prelim', 'DESC')
         ->orderBy('nilai_total', 'DESC')
-        ->orderBy('prelim_jawab_salah')
+        ->orderBy('prelim_jawab_salah', 'ASC')
         ->get()->getResult();
     }
 
