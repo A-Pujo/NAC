fc6e54e4e45b150369917ef90ce05a6519ec7eb1
akhsanfr <akhsan.fr@gmail.com>
2021-10-17 08:50:59 +0700

    apdet dashboard lomba , regis

---
+++ app/Views/test/add-data.sql
@@ -0,0 +1,12 @@
+ALTER TABLE nilai_acc_sma
+ADD `absen_3` text NOT NULL,
+ADD `absen_4` text NOT NULL,
+ADD `absen_4` text NOT NULL,
+ADD `absen_5` text NOT NULL,
+ADD `absen_6` text NOT NULL,
+ADD `absen_7` text NOT NULL,
+ADD `absen_8` text NOT NULL,
+ADD `absen_9` text NOT NULL,
+ADD `absen_10` text NOT NULL,
+ADD `absen_11` text NOT NULL
+ADD `absen_12` text NOT NULL
